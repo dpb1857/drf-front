b4f32b5115d482f7ea4850893dabee05628e956c
Don Bennett <dpb@donbennett.org>
2021-07-09 14:26:06 -0700

    Initial commit for reagent project

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java $JVM_OPTS -cp target/drffront.jar clojure.main -m drffront.server
---
+++ env/prod/cljs/drffront/prod.cljs
@@ -0,0 +1,7 @@
+(ns drffront.prod
+  (:require [drffront.core :as core]))
+
+;;ignore println statements in prod
+(set! *print-fn* (fn [& _]))
+
+(core/init!)
