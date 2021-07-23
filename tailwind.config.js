d038d4d223f3a8a9d4230cb2ee6518f2b0eca293
Don Bennett <dpb@donbennett.org>
2021-07-23 14:54:46 -0700

    adjust tailwind watch paths.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,20 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  mode: 'jit',
-  purge: {
-    // in prod look at shadow-cljs output file in dev look at runtime, which will change files that are actually compiled; postcss watch should be a whole lot faster
-    content: process.env.NODE_ENV == 'production' ? ["./public/js/main.js"] : ["./public/js/cljs-runtime/*.js"]
-  },
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
-      },
+    mode: 'jit',
+    purge: [
+        'src/cljs/**/*.cljs',
+    ],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+            },
+        },
     },
-  },
-  variants: {},
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
+    variants: {},
+    plugins: [
+        require('@tailwindcss/forms'),
+    ],
 }
